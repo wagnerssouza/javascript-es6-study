2f91183d8f239c6369b0c9a13e7a69d1fe291f21
Wagner Santos de Souza <wagner@Wagners-MacBook-Pro.local>
2016-12-20 00:29:57 -0200

    Study es6 - Map

--- forEach/foreach.js
+++ forEach/foreach.js
@@ -1,6 +1,20 @@
 (function () {
 	var colors = ['red', 'blue', 'green'];
 	colors.forEach(function (color) {
-		console.log(color);
+		//console.log(color);
 	});
+
+	var images = [
+	  { height: 10, width: 30 },
+	  { height: 20, width: 90 },
+	  { height: 54, width: 32 }
+	];
+	var areas = [];
+
+	images.forEach(function (image) {
+		areas.push(image.height * image.width);		 
+	})
+	//console.log(areas);  
+
+
 })();
---
+++ forEach/map.js
@@ -0,0 +1,24 @@
+(function () {
+	
+	var numbers = [1, 2, 3];
+	var doubledNumbers = [];
+
+	var doubled = numbers.map(function (number) {
+		return number * 2;
+	});
+
+	//console.log(doubled);
+
+	var cars = [
+		{ model: 'Buick', price: 'CHEAP' },
+		{ model: 'Camaro', price: 'Expensive' }
+	];
+
+	var prices = cars.map(function (car) {
+		return car.price;
+	})
+
+	console.log(prices);
+
+
+})();
